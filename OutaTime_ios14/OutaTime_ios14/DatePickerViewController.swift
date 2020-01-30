6a6583e9505a1ee5ccb41ac96fa451d7e5f4bae9
Stephanie Ballard <user.name>
2020-01-29 20:43:53 -0800

    .

--- OutaTime_ios14/OutaTime_ios14/DatePickerViewController.swift
+++ OutaTime_ios14/OutaTime_ios14/DatePickerViewController.swift
@@ -8,14 +8,29 @@
 
 import UIKit
 
-class DatePickerViewController: UIViewController {
+protocol DatePickerDelegate {
+    func destinationDateWasChosen(date: Date)
+}
+
+class DatePickerViewController: UIViewController, DatePickerDelegate {
+    func destinationDateWasChosen(date: Date) {
+        print(".")
+    }
+    
+    var delegate: DatePickerViewController?
 
     @IBOutlet weak var datePicker: UIDatePicker!
     
     @IBAction func cancelButton(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
     }
     
+    //For the done IBAction, call the delegate method to inform the delegate of the new date chosen by the user. Also call that same dismiss method from above after the delegate method call. This will inform the delegate of the change, and then dismiss the view, revealing the time circuits underneath.
     @IBAction func doneButton(_ sender: UIBarButtonItem) {
+//        doneButton.delegate = self
+//        doneButton.dataSource = self
+//        doneButton.delegate = self
+        dismiss(animated: true, completion: nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
--- OutaTime_ios14/OutaTime_ios14/TimeCircuitsViewController.swift
+++ OutaTime_ios14/OutaTime_ios14/TimeCircuitsViewController.swift
@@ -21,6 +21,19 @@ class TimeCircuitsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    private var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM:d:yyyy"
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        return formatter
+    }()
+    
+    //*****Set the label that shows the present time on screen to the current date using the date formatter.
+    
+    var currntSpeed = 0
+    
+//    Set the speed label to show the current speed, with "MPH" appended to the end of the string.
+//    Set the label showing the last time departed to "--- -- ----".
     
     @IBAction func setDestinationTimeButton(_ sender: UIButton) {
     }
@@ -41,3 +54,16 @@ class TimeCircuitsViewController: UIViewController {
     */
 
 }
+//********Create an extension of the class at the bottom of the file and make the class conform to the protocol from the date picker view controller (That means listing it after the VC's class name and at least stubbing out the required delegate functions).
+
+extension TimeCircuitsViewController: DatePickerDelegate {
+    func destinationDateWasChosen(date: Date) {
+        <#code#>
+    }
+    
+    
+}
+
+
+//*****Set the DatePickerViewController object's delegate as the TimeCircuitsViewController object in the prepare method.
+
